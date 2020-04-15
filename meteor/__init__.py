ada9e284b1ac1b31caba1bef066016fb8c377687
wbwseeker <10152538+wbwseeker@users.noreply.github.com>
2020-04-15 09:50:56 +0200

    add cli

--- meteor/__init__.py
+++ meteor/__init__.py
@@ -1,6 +1,15 @@
 from meteor.meteor import IdentityStage
+from meteor.meteor import Language
 from meteor.meteor import StageBase
 from meteor.meteor import StemmingStage
 from meteor.meteor import meteor
+from meteor.meteor import meteor_macro_avg
 
-__all__ = ["meteor", "StageBase", "IdentityStage", "StemmingStage"]
+__all__ = [
+    "meteor",
+    "meteor_macro_avg",
+    "Language",
+    "StageBase",
+    "IdentityStage",
+    "StemmingStage",
+]
